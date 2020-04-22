d66d3e59f4f38911837d8ae7c933d96e64b895dd
Zak Barbuto <zbarbuto@nextfaze.com>
2020-04-22 16:23:25 +0930

    Update encrypt/decrypt to support RSA

--- src/commands/encrypt.ts
+++ src/commands/encrypt.ts
@@ -1,10 +1,18 @@
-import { CipherStrategy, decodeSafe64, encryptWithKey } from '@meeco/cryppo';
+import { CipherStrategy, decodeSafe64, encryptWithKey, encryptWithPublicKey } from '@meeco/cryppo';
+import { binaryBufferToString } from '@meeco/cryppo/dist/src/util';
 import { Command, flags } from '@oclif/command';
+import { readFile } from 'fs';
+import { promisify } from 'util';
+
+const read = promisify(readFile);
 
 export default class Encrypt extends Command {
   static description = 'Encrypt a serialized encrypted value';
 
-  static examples = ['encrypt -v "hello world" -k vm8CjugMda2zdjsI9W25nH-CY-84DDYoBxTFLwfKLDk='];
+  static examples = [
+    'encrypt -v "hello world" -k vm8CjugMda2zdjsI9W25nH-CY-84DDYoBxTFLwfKLDk=',
+    'encrypt -v "hello world" -P public-key.pem'
+  ];
 
   static flags = {
     help: flags.help({ char: 'h' }),
@@ -12,20 +20,36 @@ export default class Encrypt extends Command {
     value: flags.string({ char: 'v', description: 'value to encrypt', required: true }),
     key: flags.string({
       char: 'k',
-      description: 'base64 encoded data encryption key',
-      required: true
+      description: 'base64 encoded data encryption key (if encrypting with AES)',
+      exclusive: ['publicKeyFile']
+    }),
+    publicKeyFile: flags.string({
+      char: 'P',
+      description: 'public key file (if encrypting with RSA)',
+      exclusive: ['key']
     })
   };
 
   async run() {
     const { flags } = this.parse(Encrypt);
-    const { value, key } = flags;
-    const decodedKey = decodeSafe64(key);
-    const encrypted = await encryptWithKey({
-      data: value,
-      key: decodedKey,
-      strategy: CipherStrategy.AES_GCM
-    });
-    this.log(encrypted.serialized);
+    const { value, key, publicKeyFile } = flags;
+    if (key) {
+      const decodedKey = decodeSafe64(key);
+      const encrypted = await encryptWithKey({
+        data: value,
+        key: decodedKey,
+        strategy: CipherStrategy.AES_GCM
+      });
+      this.log(encrypted.serialized);
+    } else if (publicKeyFile) {
+      const publicKeyPem = binaryBufferToString(await read(publicKeyFile));
+      const encrypted = await encryptWithPublicKey({
+        data: value,
+        publicKeyPem
+      });
+      this.log(encrypted.serialized);
+    } else {
+      this.error('Must specify either base-64 encoded encryption key or RSA public key file path');
+    }
   }
 }
