4d6504cf1bb35dd3ee90ca6059f7862bfba53b7f
Zak Barbuto <zbarbuto@nextfaze.com>
2020-04-28 15:53:01 +0930

    Format key error messages a bit nicer

--- src/handle-exception.ts
+++ src/handle-exception.ts
@@ -14,8 +14,18 @@ export async function handleException<T>(err: any, instance: Command) {
     const result = await err.json().catch(() => '');
     message = `API Responded with ${err.status}:\n ${JSON.stringify(result, null, 2)}`;
   } else if (err.message) {
-    message = err.message;
+    message = prettifyMessage(err.message);
   }
 
   return instance.error(message);
+}
+
+/**
+ * Format some common known errors in a more meaningful way
+ */
+function prettifyMessage(message: string): string {
+  if (message.includes('Invalid PEM formatted message')) {
+    return 'The private or public key file you provided was invalid';
+  }
+  return message;
 }
