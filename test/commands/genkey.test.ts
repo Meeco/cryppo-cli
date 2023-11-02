fadd485b6b508491d03e2742fdd5486631bd72e2
Yuri Leikind <yuri.leikind@meeco.me>
2023-11-02 16:27:19 +0100

    green tests for command genkey

--- test/commands/genkey.test.ts
+++ test/commands/genkey.test.ts
@@ -4,34 +4,65 @@ import { expect, test } from '@oclif/test';
 import sinon from 'sinon';
 
 describe('genkey', () => {
-  const mockKey: Uint8Array = binaryStringToBytes('vm8CjugMda2zdjsI9W25nH');
-  const base64EncodedKey = 'dm04Q2p1Z01kYTJ6ZGpzSTlXMjVuSA=='; // from encodeSafe64
+  const mockKey: Uint8Array = binaryStringToBytes('vm8CjugMda2zdjsI9W25nHfugMdapYhb');
+  const base64EncodedKey = 'dm04Q2p1Z01kYTJ6ZGpzSTlXMjVuSGZ1Z01kYXBZaGI='; // from encodeSafe64
 
   let stub;
 
   beforeEach(() => {
     stub = sinon.stub(EncryptionKey, 'generateRandom');
     stub.callsFake(() => EncryptionKey.fromBytes(mockKey));
-
   });
 
   afterEach(() => {
     stub.restore();
   });
 
+  // test.it('foo', _ctx => {
+  //   expect(true).to.equal(true);
+  //   console.log(123);
+
+  //   const d: Uint8Array = binaryStringToBytes('vm8CjugMda2zdjsI9W25nHfugMdapYhb');
+  //   console.log(d);
+
+  //   console.log(EncryptionKey.fromBytes(d).serialize);
+  // });
+
   test
     .stdout()
     .command(['genkey'])
-    .it('Generates a "random" encryption key', ctx => {
-      expect(ctx.stdout).to.contain('URL-Safe Base64 encoded key:');
+    .it('Generates a random encryption 256-bit key', ctx => {
+      expect(ctx.stdout).to.contain('256-bit URL-Safe Base64 encoded key:');
       expect(ctx.stdout).to.contain(base64EncodedKey);
     });
 
   test
+    .stdout()
+    .command(['genkey', '-l', '256'])
+    .it('Generates a random encryption 256-bit key specifying the key length', ctx => {
+      expect(ctx.stdout).to.contain('256-bit URL-Safe Base64 encoded key:');
+      expect(ctx.stdout).to.contain(base64EncodedKey);
+    });
+
+  test
+    .stdout()
+    .command(['genkey', '-l', '128'])
+    .it('Generates a random encryption 128-bit key specifying the key length', ctx => {
+      expect(ctx.stdout).to.contain('128-bit URL-Safe Base64 encoded key:');
+    });
+
+  test
+    .stdout()
+    .command(['genkey', '-l', '192'])
+    .it('Generates a random encryption 192-bit key specifying the key length', ctx => {
+      expect(ctx.stdout).to.contain('192-bit URL-Safe Base64 encoded key:');
+    });
+
+  test
     .stderr()
-    .command(['genkey', '-l', '4'])
-    .it('Provides a warning if an AES key length is specified', ctx => {
-      expect(ctx.stderr).to.contain('Warning: You have specified a key length of 4 bytes');
+    .command(['genkey', '-l', '17'])
+    .it('Provides a warning if an invalid AES key length is specified', ctx => {
+      expect(ctx.stderr).to.contain('Warning: You have specified a key length of 17 bytes');
       expect(ctx.stderr).to.contain('AES only supports');
       expect(ctx.stderr).to.contain('128, 192 or 256 bit keys');
     });
