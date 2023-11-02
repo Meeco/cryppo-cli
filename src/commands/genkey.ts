6ad1829a65fae48b3a348e95950b2ece5457fd4b
Yuri Leikind <yuri.leikind@meeco.me>
2023-11-02 16:25:16 +0100

    The default AES key size must be 256, not 128!

--- src/commands/genkey.ts
+++ src/commands/genkey.ts
@@ -14,7 +14,7 @@ export default class Genkey extends Command {
   static flags = {
     length: Flags.integer({
       char: 'l',
-      default: 128,
+      default: 256,
       description: 'length of the key in bytes to generate: 128, 192 or 256'
     })
   };
@@ -24,15 +24,15 @@ export default class Genkey extends Command {
       const { flags } = await this.parse(Genkey);
       const { length } = flags;
 
-      if (![128, 192, 256].includes(length)) {
+      if ([128, 192, 256].includes(length)) {
+        const key = EncryptionKey.generateRandom(length / 8);
+        this.log(`${length}-bit URL-Safe Base64 encoded key:`);
+        this.log(key.serialize);
+      } else {
         this.warn(
           `You have specified a key length of ${length} bytes - AES only supports 128, 192 or 256 bit keys`
         );
       }
-
-      const key = EncryptionKey.generateRandom(length / 8);
-      this.log('URL-Safe Base64 encoded key:');
-      this.log(key.serialize);
     } catch (error) {
       await handleException(error, this);
     }
